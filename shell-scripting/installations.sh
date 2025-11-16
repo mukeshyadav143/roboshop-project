22594a51ce75d918b518b0298171cb92414a1c4d
mukeshyadav143 <mukeshmandala.143681@gmail.com>
2025-11-16 21:10:22 +0530

    installations

--- shell-scripting/installations.sh
+++ shell-scripting/installations.sh
@@ -4,14 +4,18 @@ user_id=$(id -u)
 
 if [ $user_id -eq 0 ]; then 
     echo "proceeding installations wih root access"
+exit 1
 else 
     echo "please proceed wih root access"
+exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -eq 0 ]; then
     echo "mysql isntallation is success"
+exit 1
 else 
     echo "mysql installation is failure because of "0" code"
+exit 1
 fi

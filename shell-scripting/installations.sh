b0a6853cf809cdf2dc1bd48baf8baf5af34fd6b0
mukeshyadav143 <mukeshmandala.143681@gmail.com>
2025-11-16 21:23:52 +0530

    installations

--- shell-scripting/installations.sh
+++ shell-scripting/installations.sh
@@ -2,18 +2,18 @@
 
 user_id=$(id -u)
 
-if [ $user_id -eq 0 ]; then 
+if [ $user_id -ne 0 ]; then 
     echo "proceeding installations wih root access"
 else 
     echo "please proceed wih root access"
-exit 1
+    exit 1
 fi
 
-dnf install nginx -y
+dnf remove nginx -y
 
 if [ $? -ne 0 ]; then
     echo "mysql isntallation is success"
 else 
     echo "mysql installation is failure"
-exit 1
+    exit 1
 fi

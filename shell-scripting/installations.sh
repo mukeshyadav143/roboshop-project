f85de62533be004973a8955142be23c739c5c98f
mukeshyadav143 <mukeshmandala.143681@gmail.com>
2025-11-17 21:50:21 +0530

    mysql server installation

--- shell-scripting/installations.sh
+++ shell-scripting/installations.sh
@@ -1,8 +1,13 @@
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+E="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
+    echo "ERROR:: Please run this script with ${R} root privelege ${N}"
     exit 1 # failure is other than 0
 else 
     echo "Running with root previlege sudo access"

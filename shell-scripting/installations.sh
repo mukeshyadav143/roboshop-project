edeb580be6d367d3e054adc412aa200dfd719e69
mukeshyadav143 <mukeshmandala.143681@gmail.com>
2025-11-17 13:20:25 +0530

    colours in script

--- shell-scripting/installations.sh
+++ shell-scripting/installations.sh
@@ -4,21 +4,21 @@ user_id=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m"
+E="\e[0m"
 
 if [ $user_id -eq 0 ]; then 
-    echo -e "${G} proceeding installations wih root access ${N}"
+    echo -e "${G} proceeding installations wih root access ${E}"
 else 
-    echo -e "please proceed with ${R} root access ${N}"
+    echo -e "please proceed with ${R} root access ${E}"
     exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -eq 0 ]; then
-    echo -e "mysql isntallation ${G} is success ${N}"
+    echo -e "mysql isntallation ${G} is success ${E}"
 else 
-    echo -e "mysql installation is ${R} failure ${N}"
+    echo -e "mysql installation is ${R} failure ${E}"
     exit 1
 fi
 

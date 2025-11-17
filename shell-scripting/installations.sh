4ea746aba71edf73ec5485ced177e7506cdfa15f
mukeshyadav143 <mukeshmandala.143681@gmail.com>
2025-11-17 12:55:30 +0530

    colours in script

--- shell-scripting/installations.sh
+++ shell-scripting/installations.sh
@@ -1,9 +1,13 @@
 #checking the root access
 
 user_id=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+E="\e[0m"
 
 if [ $user_id -eq 0 ]; then 
-    echo "proceeding installations wih root access"
+    echo -e "$R proceeding installations wih root access $E"
 else 
     echo "please proceed wih root access"
     exit 1
